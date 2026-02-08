866043221bd40d801e027028ac8c8814d5f90fa0
Remo <remoale@icloud.com>
2026-02-08 19:12:04 -0400

    fix: NEXT_PUBLIC_VERCEL_URL

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,11 @@
 import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
-export const client = treaty<App>('localhost:3000').api
+const baseUrl =
+    typeof window !== "undefined"
+        ? window.location.origin
+        : process.env.NEXT_PUBLIC_VERCEL_URL
+            ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+            : "http://localhost:3000"
+
+export const client = treaty<App>(baseUrl).api
